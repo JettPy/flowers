c4c5a9c3205515c3745a56fe7c6a8920e392a6fd
Sergey Melnikov <55996004+JettPy@users.noreply.github.com>
2021-02-16 14:49:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    const flowersSlider = new Swiper('.flowers-slider', {
+  const flowersSlider = new Swiper(".flowers-slider", {
     // параметры слайдера
     loop: true,
     slidesPerView: 6,
 
     // Подключаем стрелки
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
 
     // настройки адаптивной версии
@@ -20,54 +20,55 @@ $(document).ready(function () {
       480: {
         slidesPerView: 3,
       },
-      // when window width is >= 992px
+      // если браузер >= 992px
       992: {
         slidesPerView: 6,
-      }
-    }
-    
+      },
+    },
   });
-  const reviewsSlider = new Swiper('.reviews-slider', {
+  const reviewsSlider = new Swiper(".reviews-slider", {
     // параметры слайдера
     loop: true,
     slidesPerView: 1,
 
     // Подключаем стрелки
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
-  $("#review-1").on('click', function() {
 
-  $.fancybox.open([
-    {
-      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
-      opts : {
-        caption : 'First caption',
-        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+  $("#review-1").on("click", function () {
+    $.fancybox.open(
+      [
+        {
+          src: "https://source.unsplash.com/IvfoDk30JnI/1500x1000",
+          opts: {
+            caption: "First caption",
+            thumb: "https://source.unsplash.com/IvfoDk30JnI/240x160",
+          },
+        },
+        {
+          src: "https://source.unsplash.com/0JYgd2QuMfw/1500x1000",
+          opts: {
+            caption: "Second caption",
+            thumb: "https://source.unsplash.com/0JYgd2QuMfw/240x160",
+          },
+        },
+        {
+          src: "https://source.unsplash.com/0JYgd2QuMfw/1500x1000",
+          opts: {
+            caption: "Second caption",
+            thumb: "https://source.unsplash.com/0JYgd2QuMfw/240x160",
+          },
+        },
+      ],
+      {
+        loop: true,
+        thumbs: {
+          autoStart: true,
+        },
       }
-    },
-    {
-      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
-      opts : {
-        caption : 'Second caption',
-        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
-      }
-    },
-    {
-      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
-      opts : {
-        caption : 'Second caption',
-        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
-      }
-    }
-  ], {
-    loop : true,
-    thumbs : {
-      autoStart : true
-    }
+    );
   });
-
-});
 });
